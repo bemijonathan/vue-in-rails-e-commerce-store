c4b9a0b48213759dcab55aaf129c2fc1e86dd637
bemijonathan <bemijonathan@gmail.com>
2020-02-04 07:08:17 +0100

    unable to fix jquery error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,34 +3,41 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// =require_tree public
+
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("/js/vendor/modernizr-3.5.0.min.js")
+// require('js/vendor/modernizr-3.5.0.min.js')
+window.jQuery = window.jquery = require("js/vendor/jquery-3.2.1.min.js")
+require("js/plugins.js")
+require("js/active.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
+// or the `imagePath` JavaScript helper nb   d 
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
 
-import TurbolinksAdapter from 'vue-turbolinks'
+
+
+
+import user from '../users.vue'
+import Index from "../index.vue"
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
-import user from '../users.vue'
-Vue.use(TurbolinksAdapter)
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#hello',
-    data: () => {
-      return {
-        message: "Can you say hello?"
-      }
+    data: {
+      message: "Can you say hello?"
     },
-    components: { App, user }
+    components: { App, user , Index }
   })
 })
